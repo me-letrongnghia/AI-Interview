35167138a230457e8d8d5a56ed4d2b81c2ac800e
me-letrongnghia <nghialee2301@gmail.com>
2025-08-13 23:11:44 +0700

    Setup project & docker-compose

---
+++ backend/ai-interview-be/src/main/java/com/capstone/ai_interview_be/AiInterviewBeApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.ai_interview_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewBeApplication.class, args);
+	}
+
+}
