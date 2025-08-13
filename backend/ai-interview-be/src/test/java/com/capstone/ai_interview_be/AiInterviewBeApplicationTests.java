bad7bd038e1dd4a2ed5181257f50ded2a86176b1
me-letrongnghia <nghialee2301@gmail.com>
2025-08-13 23:11:44 +0700

    Setup project & docker-compose

---
+++ backend/ai-interview-be/src/test/java/com/capstone/ai_interview_be/AiInterviewBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ai_interview_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
