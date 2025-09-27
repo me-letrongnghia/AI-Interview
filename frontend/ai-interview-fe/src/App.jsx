4b6674663b790e32e4505566b9679f7ece16edaf
Le Nguyen To <lenguyento61@gmail.com>
2025-09-27 16:15:41 +0700

    config tailwindcss for fontend

--- frontend/ai-interview-fe/src/App.jsx
+++ frontend/ai-interview-fe/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="text-gray-200 ">
+        Hello world!
+        <div className="text-blue-700 ">
+          xin chao cac ban
+        </div>
+      </h1>
+      <div className="">
+        <h2 className="text-2xl font-semibold text-red-500"></h2>
+        <div className="text-blue-700">âcxzc</div>
+        <ul className="list-disc list-inside">
+          <li className="text-red-500">đá</li>
+          <li className="text-red-500">ádas</li>
+          <li className="text-red-500">ádsa</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
