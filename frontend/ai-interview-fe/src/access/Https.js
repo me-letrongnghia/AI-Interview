9d9e414c47b2d6e9741bab492e6cde7ac7e9766f
Le Nguyen To <lenguyento61@gmail.com>
2025-09-29 09:22:10 +0700

    Add class call API

---
+++ frontend/ai-interview-fe/src/access/Https.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+class Http {
+  static instance = axios.create({
+    baseURL: "http://localhost:8080",
+    timeout: 10000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+// interceptor request
+Http.instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// interceptor response
+Http.instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.error("Unauthorized, please login again.");
+    }
+    return Promise.reject(error);
+  }
+);
+const Https = Http.instance;
+export default Https;
