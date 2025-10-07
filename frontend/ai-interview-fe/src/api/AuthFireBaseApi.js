92954a17f0f7c46a2bef9f556bd59ce70009b533
Le Nguyen To <lenguyento61@gmail.com>
2025-10-07 16:43:42 +0700

    speech API

---
+++ frontend/ai-interview-fe/src/api/AuthFireBaseApi.js
@@ -0,0 +1,40 @@
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider, githubProvider } from "../config/firebaseConfig";
+
+//Google Login
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    const user = result.user;
+    const tokenFirebase = await user.getIdToken();
+
+    console.log("Đăng nhập Google thành công:", {
+      email: user.email,
+      tokenFirebase,
+    });
+
+    return { success: true, data: { email: user.email || "", tokenFirebase } };
+  } catch (error) {
+    console.error("Lỗi đăng nhập Google:", error);
+    return { success: false, error: "Đăng nhập Google thất bại" };
+  }
+};
+
+//GitHub Login
+export const signInWithGithub = async () => {
+  try {
+    const result = await signInWithPopup(auth, githubProvider);
+    const user = result.user;
+    const tokenFirebase = await user.getIdToken();
+
+    console.log("Đăng nhập GitHub thành công:", {
+      email: user.email,
+      tokenFirebase,
+    });
+
+    return { success: true, data: { email: user.email || "", tokenFirebase } };
+  } catch (error) {
+    console.error("Lỗi đăng nhập GitHub:", error);
+    return { success: false, error: "Đăng nhập GitHub thất bại" };
+  }
+};
